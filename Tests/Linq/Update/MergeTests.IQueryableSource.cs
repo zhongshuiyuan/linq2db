0afd6292e6b3f5b16efb71f5cf7d541e2ab59a0a
Stuart Turner <stuart@turner-isageek.com>
2020-07-25 13:35:23 -0500

    Add source Cte test

--- Tests/Linq/Update/MergeTests.IQueryableSource.cs
+++ Tests/Linq/Update/MergeTests.IQueryableSource.cs
@@ -135,5 +135,35 @@ namespace Tests.xUpdate
 				AssertRow(InitialSourceData[3], result[5], null, 216);
 			}
 		}
+
+		[Test]
+		public void MergeUsingCte([MergeDataContextSource] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				PrepareData(db);
+
+				var table = GetTarget(db);
+
+				var rows = table
+					.Merge().Using(GetSource1(db).Where(_ => _.Id >= 1).AsCte())
+					.OnTargetKey()
+					.InsertWhenNotMatched()
+					.Merge();
+
+				var result = table.OrderBy(_ => _.Id).ToList();
+
+				AssertRowCount(2, rows, context);
+
+				Assert.AreEqual(6, result.Count);
+
+				AssertRow(InitialTargetData[0], result[0], null, null);
+				AssertRow(InitialTargetData[1], result[1], null, null);
+				AssertRow(InitialTargetData[2], result[2], null, 203);
+				AssertRow(InitialTargetData[3], result[3], null, null);
+				AssertRow(InitialSourceData[2], result[4], null, null);
+				AssertRow(InitialSourceData[3], result[5], null, 216);
+			}
+		}
 	}
 }
